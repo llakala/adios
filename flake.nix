aa262b46092db1641d1250a468425e0e95cb0e13
adisbladis <adisbladis@gmail.com>
2024-11-21 13:24:23 +0000

    Repair flake

--- flake.nix
+++ flake.nix
@@ -2,6 +2,13 @@
   description = "Adios";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    korora.url = "github:adisbladis/korora";
+    korora.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { nixpkgs }: import ./. { inherit (nixpkgs) lib; };
+  outputs =
+    { nixpkgs, korora, ... }:
+    import ./. {
+      korora = korora.lib;
+      inherit (nixpkgs) lib;
+    };
 }
