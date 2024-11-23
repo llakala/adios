3b6774885f100796b46c620f60a95124eed8b045
adisbladis <adisbladis@gmail.com>
2024-11-23 01:53:03 +0000

    contrib: Test type

--- contrib/default.nix
+++ contrib/default.nix
@@ -7,6 +7,18 @@
     pkgs.type = types.attrs;
   };
 
+  tests = {
+    testType = {
+      expr = types.modules.module.verify (
+        self
+        // {
+          # Don't test the shape of tests because of infinite recursion
+        }
+      );
+      expected = null;
+    };
+  };
+
   modules = {
     nixos = import ./modules/nixos.nix;
     treefmt = import ./modules/treefmt {
