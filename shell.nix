412615870fb76636555d5e71748cc9a748b56c1e
adisbladis <adisbladis@gmail.com>
2025-08-07 20:13:32 +1200

    Bump inputs, drop multiple nix-unit impls

--- shell.nix
+++ shell.nix
@@ -1,46 +1,17 @@
 {
-  nixImpl ? "nix",
   pkgs ? import __sources.nixpkgs { },
   __sources ? import ./npins,
 }:
 
 let
-  sources = __sources;
-
   inherit (pkgs) lib;
-
-  testImpls = {
-    nix = pkgs.nix-unit;
-
-    lix =
-      let
-        lix = pkgs.callPackage "${sources.lix}/package.nix" {
-          stdenv = pkgs.clangStdenv;
-        };
-
-        lix-unit =
-          (pkgs.nix-unit.override {
-            # Hacky overriding :)
-            nixVersions = lib.mapAttrs (_n: _v: lix) pkgs.nixVersions;
-            # nix = pkgs.lixVersions.latest;
-          }).overrideAttrs
-            (_old: {
-              pname = "lix-unit";
-              name = "lix-unit-${lix.version}";
-              inherit (lix) version;
-              src = sources.lix-unit;
-            });
-      in
-      lix-unit;
-  };
-
   dev = import ./dev { inherit __sources pkgs lib; };
 
 in
 pkgs.mkShell {
   packages = [
     pkgs.npins
-    testImpls.${nixImpl}
+    pkgs.nix-unit
     dev.treefmt
     pkgs.mdbook
   ];
