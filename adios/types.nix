08b30dd32590cef001e17364a1eebf2d51f60b29
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-11-17 13:48:59 -0500

    adios: only use `let` recursion for module types
    More clear than using a big odd mix of `//` and rec, where it's not
    always clear where something is coming from.

--- adios/types.nix
+++ adios/types.nix
@@ -2,104 +2,104 @@
 
 let
   inherit (builtins) isString;
-  inherit (types)
+  inherit (korora)
+    any
+    attrs
     attrsOf
-    union
-    struct
+    function
+    never
+    option
     optionalAttr
     string
-    never
-    function
+    struct
     type
-    any
-    modules
+    typedef
+    typedef'
+    union
     ;
 
   neverAttr = optionalAttr never;
 
   typesT = attrsOf modules.typedef;
 
-  types = korora // {
-    modules = rec {
-      typedef =
-        types.typedef' "typedef"
-          (union [
-            function
-            type
-            typesT
-          ]).verify;
+  modules = {
+    typedef =
+      typedef' "typedef"
+        (union [
+          function
+          type
+          typesT
+        ]).verify;
 
-      option =
-        (struct "option" {
-          inherit type;
-          default = optionalAttr any;
-          defaultFunc = optionalAttr types.function;
-          description = optionalAttr string;
-        }).override
-          {
-            verify =
-              option:
-              if option ? default && option ? defaultFunc then
-                "'default' & 'defaultFunc' are mutually exclusive"
-              else
-                null;
-          };
-
-      subOptions = struct "subOptions" {
-        inherit options;
-        description = optionalAttr string;
-        # Make fields used for normal options non-permitted
-        type = neverAttr;
-        default = neverAttr;
-        defaultFunc = neverAttr;
-      };
-
-      options = attrsOf (union [
-        modules.option
-        modules.subOptions
-      ]);
-
-      input = types.option (
-        attrsOf (
-          struct "input" {
-            # Note: The lack of a type for an input means no type checking done.
-            type = optionalAttr type;
-            # TODO: Narrow permitted chars
-            path = types.typedef "pathstring" isString;
-          }
-        )
-      );
-
-      inputs = attrsOf input;
-
-      lib = types.attrs;
-
-      moduleDef =
-        (struct "moduleDef" {
-          name = optionalAttr string;
-          modules = optionalAttr (attrsOf module);
-          types = optionalAttr typesT;
-          impl = optionalAttr function;
-          options = optionalAttr options;
-          inputs = optionalAttr inputs;
-        }).override
-          {
-            verify =
-              self:
-              (if self ? type && self ? options then "'type' is mutually exclusive with 'options'" else null);
-          };
-
-      module = struct "module" {
-        name = optionalAttr string;
-        modules = attrsOf module;
-        types = typesT;
-        inherit options;
+    option =
+      (struct "option" {
         inherit type;
-        inherit inputs;
-        __functor = optionalAttr function;
-      };
+        default = optionalAttr any;
+        defaultFunc = optionalAttr function;
+        description = optionalAttr string;
+      }).override
+        {
+          verify =
+            option:
+            if option ? default && option ? defaultFunc then
+              "'default' & 'defaultFunc' are mutually exclusive"
+            else
+              null;
+        };
+
+    subOptions = struct "subOptions" {
+      inherit (modules) options;
+      description = optionalAttr string;
+      # Make fields used for normal options non-permitted
+      type = neverAttr;
+      default = neverAttr;
+      defaultFunc = neverAttr;
+    };
+
+    options = attrsOf (union [
+      modules.option
+      modules.subOptions
+    ]);
+
+    input = option (
+      attrsOf (
+        struct "input" {
+          # Note: The lack of a type for an input means no type checking done.
+          type = optionalAttr type;
+          # TODO: Narrow permitted chars
+          path = typedef "pathstring" isString;
+        }
+      )
+    );
+
+    inputs = attrsOf modules.input;
+
+    lib = attrs;
+
+    moduleDef =
+      (struct "moduleDef" {
+        name = optionalAttr string;
+        modules = optionalAttr (attrsOf modules.module);
+        types = optionalAttr typesT;
+        impl = optionalAttr function;
+        options = optionalAttr modules.options;
+        inputs = optionalAttr modules.inputs;
+      }).override
+        {
+          verify =
+            self:
+            (if self ? type && self ? options then "'type' is mutually exclusive with 'options'" else null);
+        };
+
+    module = struct "module" {
+      name = optionalAttr string;
+      modules = attrsOf modules.module;
+      types = typesT;
+      inherit (modules) options inputs;
+      inherit type;
+      __functor = optionalAttr function;
     };
   };
 
 in
-types
+korora // { inherit modules; }
