e73bcfdf186ff7dd7456be59d9418f084a2a88e0
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-03-18 15:32:58 -0400

    types: remove korora dev tooling
    I don't plan on maintaining korora in its own repo, instead pushing
    changes directly to the `types/` folder. We move much of the dev tooling
    to the root, including an envrc.

--- shell.nix
+++ shell.nix
@@ -1,13 +1,24 @@
 {
-  pkgs ? import __sources.nixpkgs {},
+  pkgs ? import __sources.nixpkgs { },
   __sources ? import ./npins,
 }:
+
+let
+  inherit (pkgs) lib;
+  # generates docs for korora
+  mkdocs = pkgs.writeShellScriptBin "mkdocs" ''
+    ${lib.getExe' pkgs.nixdoc "nixdoc"} --category types --description "Kororā" --file types/types.nix | sed s/' {#.*'/""/ > types/README.md
+  '';
+in
 
 pkgs.mkShell {
   packages = [
     pkgs.npins
-    pkgs.nix-unit
     pkgs.mdbook
     pkgs.mdbook-cmdrun
+
+    pkgs.nix-unit
+    pkgs.nixdoc
+    mkdocs
   ];
 }
