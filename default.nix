1e2cb1789233a998151a3a9346af8ef9467c4aef
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2026-03-18 15:47:12 -0400

    add deprecation notice of branch

--- default.nix
+++ default.nix
@@ -1,4 +1,7 @@
 rec {
-  adios = import ./adios;
+  adios = (builtins.abort or builtins.warn) ''
+    The providers-and-consumers branch of my Adios fork has been deprecated.
+    Please instead point to the master branch of llakala/adios.
+  '' import ./adios;
   adios-contrib = import ./contrib adios;
 }
